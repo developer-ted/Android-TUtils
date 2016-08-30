0e926e44fac3bc97026784c1a541434a034406df
Adel <Adel@madsq.net>
2016-08-30 11:34:32 +0900

    add method set image with Glide library on ImageUtils

--- tutils/src/main/java/com/tedkim/android/tutils/ImageUtils.java
+++ tutils/src/main/java/com/tedkim/android/tutils/ImageUtils.java
@@ -10,11 +10,14 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Build;
+import android.provider.ContactsContract;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
 import android.renderscript.RenderScript;
 import android.renderscript.ScriptIntrinsicBlur;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.facebook.cache.common.SimpleCacheKey;
 import com.facebook.drawee.backends.pipeline.Fresco;
 import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
@@ -135,6 +138,31 @@ public class ImageUtils {
                         view.setImageURI(Uri.EMPTY);
                     else
                         view.setImageURI(Uri.parse(url));
+                }
+            }
+        });
+    }
+
+    /**
+     * Adel, 2016-08-29
+     * image setting with Glide
+     *
+     * @param view ImageView
+     * @param url  image url
+     * @param res  default image resource id
+     */
+    public static void setImage(final ImageView view, final String url, final int res) {
+        view.post(new Runnable() {
+            @Override
+            public void run() {
+                if (url == null) {
+                    Glide.with(view.getContext()).load(res).into(view);
+                } else {
+                    if (url.length() == 0)
+                        Glide.with(view.getContext()).load(res).into(view);
+                    else
+                        Glide.with(view.getContext()).load(url).into(view);
+
                 }
             }
         });
